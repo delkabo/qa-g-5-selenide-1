2eced719e864c1f6d3431389609dc04482e143a6
delkabo <delkabo@yandex.ru>
2022-03-06 19:46:32 +0300

    5 Урок Селенид. ДЗ 1. Первый коммит

--- src/test/java/git/SearchSelenideRepo.java
+++ src/test/java/git/SearchSelenideRepo.java
@@ -1,8 +1,27 @@
 package git;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SearchSelenideRepo {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+    }
+
     @Test
+    void openGitSelenide() {
+        open("/selenide/selenide");
+        $("[data-content='Wiki']").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        $(byText("SoftAssertions")).click();
+        $$("h4").findBy(text("Using JUnit5 extend test class")).sibling(0)
+               .shouldHave(text("class Tests"));
+    }
 
 }
